0c523aa4f120c9a31539b9304899323d245e2b5c
Shantanu Shinde <shantanushinde36@gmail.com>
2022-11-11 14:06:06 +0530

    Add files via upload

---
+++ src/app/create-patient/create-patient.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Patient } from '../patient';
+import { PatientService } from '../patient.service';
+
+@Component({
+  selector: 'app-create-patient',
+  templateUrl: './create-patient.component.html',
+  styleUrls: ['./create-patient.component.css']
+})
+export class CreatePatientComponent implements OnInit {
+
+  patient:Patient = new Patient();
+  constructor(private patientService:PatientService,private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  savePatient(){
+    this.patientService.createPatient(this.patient).subscribe(data=>{
+      console.log(data);
+      this.gotoPatientList();
+    },
+    error => console.log(error));
+  }
+
+  gotoPatientList(){
+    this.router.navigate(['/patients']);
+  }
+
+  onSubmit(){
+    console.log(this.patient);
+    this.savePatient()
+  }
+}
---
+++ src/app/patient.ts
@@ -0,0 +1,10 @@
+export class Patient {
+
+    id!: number;
+    insuranceCompany!: string;
+    patientId!: number;
+    patientName!: string;
+    age!: number;
+    drName!: string;
+   
+}
